3458299ebffb2a8c52452fa7165fdb48bee1119e
Chrustkiran <christkiran.15@cse.mrt.ac.lk>
2020-09-08 01:07:17 +0530

    added map for getting diseases

--- src/main/java/com/med/symptom/service/Disease.java
+++ src/main/java/com/med/symptom/service/Disease.java
@@ -8,8 +8,25 @@ import java.util.HashSet;
 
 public interface Disease {
     String OVUL_DISORDER = "OvulatoryDisorder";
+    String ENDOMETRIOSIS = "Endometriosis";
+    String HYPOTHALAMO_FAILURE = "HypothalamoPituitaryFailure";
+    String POLY_CYSTIC_SYNDROME = "PolycysticOverianSyndrome";
+    String TUBAL_DISORDER = "TubalDisorder";
+    String PREMATURE_FAILURE = "PrematureOverianFailure";
+
+    String FAMILY_HISTORY = "Family_History";
+    String MENSURAL_HISTORY_PAIN = "Mensural_History_Pain";
+    String PRE_HIST_PELVIC = "Previous_Historyof_Pelvic_Inflammatory_Disease";
+
+
+
     String FINDS = "finds";
     String FIND_BY = "findby";
+    HashMap<String, String> diseaseMedHistMap = new HashMap<String, String>(){{
+        put(FAMILY_HISTORY, HYPOTHALAMO_FAILURE + ',' + POLY_CYSTIC_SYNDROME + ',' + PREMATURE_FAILURE);
+        put(MENSURAL_HISTORY_PAIN, ENDOMETRIOSIS);
+        put(PRE_HIST_PELVIC, TUBAL_DISORDER);
+    }};
     ArrayList<String> getAllOvulatoryDisorder();
     HashMap<String, HashSet<DiseaseDTO>> getDiseaseTestMap(HashMap<String, String> infoBody);
 }
--- src/main/java/com/med/symptom/service/DiseaseImpl.java
+++ src/main/java/com/med/symptom/service/DiseaseImpl.java
@@ -7,13 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 
 @Service
 public class DiseaseImpl extends SymptomService implements Disease {
-    private final static String YES = "yes";
-    private final static String NO = "no";
+    private final static String YES = "Yes";
+    private final static String NO = "No";
     private final static String ALCOHOL = "Alcohol";
     private final static String SMOKING = "Smoking";
 
@@ -26,25 +27,23 @@ public class DiseaseImpl extends SymptomService implements Disease {
     public HashMap<String, HashSet<DiseaseDTO>> getDiseaseTestMap(HashMap<String, String> infoBody) {
         HashMap<String, HashSet<DiseaseDTO>> diseaseTestMap = new HashMap<>();
         if (!owlReader.getObjectProperties().isEmpty()) {
-            if (YES.equalsIgnoreCase(infoBody.get(ALCOHOL)) && YES.equals(infoBody.get(SMOKING))) {
                 for (OWLObjectDTO owlProp : owlReader.getObjectProperties()) {
-                    if (FINDS.equals(owlProp.getSuperObjectProperty())) {
-                        DiseaseDTO diseaseDTO = new DiseaseDTO();
-                        diseaseDTO.setDisease(owlProp.getRange());
-                        diseaseDTO.setTest(owlProp.getDomain());
-                        diseaseDTO.setPositiveResult(owlProp.getObjectProperty());
+                   if (FIND_BY.equals(owlProp.getSuperObjectProperty())) {
+                       for (String key: Disease.diseaseMedHistMap.keySet()) {
+                           if (YES.equalsIgnoreCase(infoBody.get(key))
+                                   && Arrays.asList(Disease.diseaseMedHistMap.get(key)
+                                   .split(",")).contains(owlProp.getDomain())) {
+                               DiseaseDTO diseaseDTO = new DiseaseDTO();
+                               diseaseDTO.setDisease(owlProp.getDomain());
+                               diseaseDTO.setTest(owlProp.getRange());
+                               diseaseDTO.setPositiveResult(owlProp.getObjectProperty());
 
-                        SetUtlil.putValueinHashMap(diseaseTestMap, diseaseDTO, diseaseDTO.getDisease());
-                    } else if (FIND_BY.equals(owlProp.getSuperObjectProperty())) {
-                        DiseaseDTO diseaseDTO = new DiseaseDTO();
-                        diseaseDTO.setDisease(owlProp.getDomain());
-                        diseaseDTO.setTest(owlProp.getRange());
-                        diseaseDTO.setPositiveResult(owlProp.getObjectProperty());
-
-                        SetUtlil.putValueinHashMap(diseaseTestMap, diseaseDTO, diseaseDTO.getDisease());
+                               SetUtlil.putValueinHashMap(diseaseTestMap, diseaseDTO, diseaseDTO.getDisease());
+                           }
+                       }
                     }
                 }
-            }
+
         }
 
         return diseaseTestMap;
